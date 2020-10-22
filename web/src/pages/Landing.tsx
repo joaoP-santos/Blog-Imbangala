61527cdd78d624e6852a9b8782714fb14051bd2a
João Pedro Santos <66499620+joaoP-santos@users.noreply.github.com>
2020-10-22 17:32:47 -0300

    Completed navigation bar

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+import '../styles/landing.css'
+
+function Landing() {
+  return (
+    <div id="landing">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Landing;
