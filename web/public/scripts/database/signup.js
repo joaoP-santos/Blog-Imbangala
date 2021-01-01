a473d7b5753a747e87be026e7b3a3f7fcc625874
João Pedro Santos <66499620+joaoP-santos@users.noreply.github.com>
2021-01-01 12:20:16 -0300

    Finished sign-up page (added database interaction)

---
+++ web/public/scripts/database/signup.js
@@ -0,0 +1,48 @@
+let name = document.querySelector("#name")
+let email = document.querySelector("#email")
+let password = document.querySelector("#password")
+let confirmPassword = document.querySelector("#confirm-password")
+
+let output = document.querySelector("#output")
+let form = document.querySelector("form")
+
+form.addEventListener("submit", signUp)
+function signUp() {
+   if (password.value == confirmPassword.value) {
+      firebase
+         .auth()
+         .createUserWithEmailAndPassword(email.value, password.value)
+         .then((user) => {
+            const database = firebase.firestore()
+
+            const date = new Date()
+            const createdAt = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`
+
+            database.doc(`/users/${user.user.uid}`).set({
+               name: name.value,
+               email: user.user.email,
+               createdAt: createdAt,
+            })
+            output.innerHTML = "Conta criada com sucesso."
+            setTimeout(function () {
+               window.location = "/"
+            }, 3000)
+         })
+         .catch((error) => {
+            var errorCode = error.code
+            var errorMessage = error.message
+            console.log(`ERROR: ${errorMessage}
+  code ${errorCode}`)
+            switch (errorCode) {
+               case "auth/weak-password":
+                  output.innerHTML = "A senha é fraca demais."
+                  break
+               case "auth/email-already-in-use":
+                  output.innerHTML =
+                     "O email inserido já está em uso por outra conta."
+            }
+         })
+   } else {
+      output.innerHTML = "As senhas não coincidem."
+   }
+}
